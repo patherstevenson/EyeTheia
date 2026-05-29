072d8ee7571461eb05bf7d451adb00b4ace86943
Clément Cardon <clement.cardon3.etu@univ-lille.fr>
2026-05-29 16:13:56 +0200

    HeatMap in Result Screen

--- src/ui/views/ResultsScreenFlet.py
+++ src/ui/views/ResultsScreenFlet.py
@@ -13,7 +13,7 @@ class ExperiencePreview(ft.Container):
 
         self.word_preview = WordPreview(words=res.words.words, size=24, font=ft.FontWeight.W_600, spacing=16)
         self.points_preview = PointsCanva(res)
-        # self.score_preview = WordsWidget_old(res.gaze_score, 18, ft.FontWeight.W_600, 8, 4, ft.Colors.BLUE)
+        self.heatmap = HeatMap(res)
 
         self.content = self.word_preview
 
@@ -25,14 +25,115 @@ class ExperiencePreview(ft.Container):
         self.on_click = self.handle_click
 
     def handle_click(self, e):
+        self.state += 1
         self.state = self.state % 3
         if self.state == 0:
-            self.content = self.points_preview
-        elif self.state == 1:
             self.content = self.word_preview
+        elif self.state == 1:
+            self.content = self.points_preview
+        elif self.state == 2:
+            self.content = self.heatmap
 
-        self.state += 1
         self.update()
+
+
+@ft.control
+class PointsCanva(ft.Container):
+    def __init__(self, res: GroupResults, **kwargs):
+        super().__init__(**kwargs)
+
+        self.res = res
+        self.built = False
+
+        self.canva = cv.Canvas(
+            on_resize=self.handle_resize
+        )
+
+        self.content = self.canva
+        self.expand = 1
+        self.aspect_ratio = 16 / 9
+        self.data = True
+
+    def build(self):
+        self.built = True
+
+    def handle_resize(self, e):
+        """Handle when the window resize to update all widgets"""
+        canva_width: float = e.width
+        canva_height: float = e.height
+
+        if self.built:
+            page = self.page
+
+            shapes = []
+
+            old_x = -1
+            old_y = -1
+
+            style = ft.TextStyle(size=10)
+
+            for pt in self.res.gaze_points:
+                # stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE, color=ft.Colors.random(exclude=[ft.Colors.WHITE, ft.Colors.GREY]))
+                stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE, color=ft.Colors.BLACK)
+
+                x = round((pt.x / SCREEN_WIDTH) * canva_width)
+                y = round((pt.y / SCREEN_HEIGHT) * canva_height)
+                shapes.append(cv.Circle(x=x, y=y, radius=10, paint=stroke_paint))
+                shapes.append(cv.Text(x=x, y=y, value=str(pt.index), style=style, alignment=ft.Alignment.CENTER))
+                if old_x >= 0 & old_y >= 0:
+                    shapes.append(cv.Line(paint=stroke_paint, x1=old_x, y1=old_y, x2=x, y2=y))
+                old_x = x
+                old_y = y
+
+            e.control.shapes = shapes
+
+            page.update()
+
+
+@ft.control
+class HeatMap(cv.Canvas):
+    def __init__(self, res: GroupResults, **kwargs):
+        super().__init__(**kwargs)
+
+        self.res = res
+        self.shapes = []
+
+        self.canva_width = 1920
+        self.canva_height = 1080
+
+        self.on_resize = self.handle_resize
+
+    def handle_resize(self, e):
+        """Handle when the window resize to update all widgets"""
+        self.canva_width: float = e.width
+        self.canva_height: float = e.height
+        self.draw_heatmap()
+
+    def draw_heatmap(self):
+        self.shapes.clear()
+
+        for pt in self.res.gaze_points:
+            x = round((pt.x / SCREEN_WIDTH) * self.canva_width)
+            y = round((pt.y / SCREEN_HEIGHT) * self.canva_height)
+
+            paint_heatmap = ft.Paint(
+                gradient=ft.PaintRadialGradient(
+                    center=(x, y),
+                    radius=50,
+                    colors=[
+                        ft.Colors.with_opacity(0.4, ft.Colors.RED_ACCENT_400),
+                        ft.Colors.with_opacity(0.2, ft.Colors.ORANGE_300),
+                        ft.Colors.with_opacity(0.0, ft.Colors.ORANGE),
+                        # ft.Colors.TRANSPARENT,
+                    ],
+                    color_stops=[0.0, 0.5, 1.0]
+                )
+            )
+            self.shapes.append(
+                cv.Circle(
+                    x, y, 50, paint=paint_heatmap
+                )
+            )
 
 
 @ft.control
@@ -55,41 +156,6 @@ class WordPreview(ft.Column):
         self.alignment = ft.MainAxisAlignment.CENTER
         self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         self.spacing = spacing
-        self.expand = 1
-        self.aspect_ratio = 1
-
-
-@ft.control
-class WordsWidget_old(ft.Container):
-    """Left part of a word_group widget, with words or points"""
-
-    def __init__(self, data, size, font: ft.FontWeight, spacing, border_width, border_color, is_score: bool = False, **kwargs):
-        super().__init__(**kwargs)
-
-        words = []
-        for o in data:
-            words.append(str(o))
-
-        self.content = ft.Column(
-            controls=[
-                ft.Row(
-                    controls=[ft.Text(words[0], size=size, weight=font),
-                              ft.Text(words[1], size=size, weight=font)],
-                    alignment=ft.MainAxisAlignment.SPACE_EVENLY,
-
-                ),
-                ft.Row(
-                    controls=[ft.Text(words[2], size=size, weight=font),
-                              ft.Text(words[3], size=size, weight=font)],
-                    alignment=ft.MainAxisAlignment.SPACE_EVENLY,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=spacing,
-            data=is_score
-        )
-        self.border = ft.Border.all(border_width, border_color)
         self.expand = 1
         self.aspect_ratio = 1
 
@@ -147,18 +213,6 @@ class DataWidget(ft.Column):
         self.aspect_ratio = 1
 
 
-def switch_infos(word_tab_widget: ft.Control, res: GroupResults, page):
-    """Switch the main tab between the 4 words and a visualization of where the user looked"""
-    if word_tab_widget.content.data:
-        word_tab_widget.content = WordsWidget_old(res.words.words, 24, ft.FontWeight.W_600, 16, 6, ft.Colors.BLUE, False).content
-        page.update()
-    else:
-        word_tab_widget.content = PointsCanva(page, res)
-        page.update()
-
-    word_tab_widget.on_click = lambda _: switch_infos(word_tab_widget, res, page)
-
-
 @ft.control
 class ResWidget(ft.Container):
     """Widget used to show results of a single word_group"""
@@ -190,59 +244,6 @@ class ResWidget(ft.Container):
         self.border = ft.Border.all(10, ft.Colors.BLUE_ACCENT)
         self.expand = 1
         self.margin = ft.Margin.symmetric(horizontal=20)
-
-
-@ft.control
-class PointsCanva(ft.Container):
-    def __init__(self, res: GroupResults, **kwargs):
-        super().__init__(**kwargs)
-
-        self.res = res
-        self.built = False
-
-        self.canva = cv.Canvas(
-            on_resize=self.handle_resize
-        )
-
-        self.content = self.canva
-        self.expand = 1
-        self.aspect_ratio = 16 / 9
-        self.data = True
-
-    def build(self):
-        self.built = True
-
-    def handle_resize(self, e):
-        """Handle when the window resize to update all widgets"""
-        canva_width: float = e.width
-        canva_height: float = e.height
-
-        if self.built:
-            page = self.page
-
-            shapes = []
-
-            old_x = -1
-            old_y = -1
-
-            style = ft.TextStyle(size=10)
-
-            for pt in self.res.gaze_points:
-                # stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE, color=ft.Colors.random(exclude=[ft.Colors.WHITE, ft.Colors.GREY]))
-                stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE, color=ft.Colors.BLACK)
-
-                x = round((pt.x / SCREEN_WIDTH) * canva_width)
-                y = round((pt.y / SCREEN_HEIGHT) * canva_height)
-                shapes.append(cv.Circle(x=x, y=y, radius=10, paint=stroke_paint))
-                shapes.append(cv.Text(x=x, y=y, value=str(pt.index), style=style, alignment=ft.Alignment.CENTER))
-                if old_x >= 0 & old_y >= 0:
-                    shapes.append(cv.Line(paint=stroke_paint, x1=old_x, y1=old_y, x2=x, y2=y))
-                old_x = x
-                old_y = y
-
-            e.control.shapes = shapes
-
-            page.update()
 
 
 @ft.control
